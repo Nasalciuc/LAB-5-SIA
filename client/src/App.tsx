41d746028b9689a878e84ea7c153a00f57a970ed
Mr.Ordo <166841033+Nasalciuc@users.noreply.github.com>
2026-03-12 18:48:21 +0200

    Remove useLocation and always show Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -7,8 +7,6 @@ import Services from './pages/Services'
 import Contact from './pages/Contact'
 
 function App() {
-  const location = useLocation()
-
   return (
     <div className="app">
       <Navbar />
@@ -20,7 +18,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
-      {location.pathname === '/' && <Footer />}
+      <Footer />
     </div>
   )
 }
